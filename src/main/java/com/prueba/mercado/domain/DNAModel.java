eb461e3bb60cc0f569c7ba19693d595bca608194
bastian113 <sebasar751@gmail.com>
2021-09-25 08:35:05 -0500

    Add creation date to sequences

--- src/main/java/com/prueba/mercado/domain/DNAModel.java
+++ src/main/java/com/prueba/mercado/domain/DNAModel.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.Type;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.sql.Timestamp;
 import java.util.UUID;
 
 @Data
@@ -27,5 +29,9 @@ public class DNAModel {
 
     @Column(nullable = false, updatable = false)
     private Boolean isMutant;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private Timestamp creationDate;
 }
 
